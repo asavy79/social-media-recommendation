4cc8007ef43beaae08cb49fa9ddecacdbbcee47e
Jake Lewis <143766387+dapig-21@users.noreply.github.com>
2026-04-29 12:16:51 -0600

    Added top N most commonly shared features function
    Refactor visualization functions to improve clarity and add functionality. Introduced new parameters and updated logic for handling graph visualizations and common traits.

--- src/Visualize.py
+++ src/Visualize.py
@@ -18,17 +18,27 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import Counter
+import pandas as pd
+from tabulate import tabulate
 
-def vizsualize_graph(G, recommended):
-    H = G.subgraph(t[0] for t in recommended)
-    pos = nx.spring_layout(H, seed=42)
-    edge_labels = nx.get_edge_attributes(G, "score")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+def visualize_graph(G, recommended, target):
+    nodes = [t[0] for t in recommended]
+    
+    H = G.subgraph(nodes).copy()
+   
+
+    pos = nx.spring_layout(H, seed=42,  k=1.5)
+    edge_labels = nx.get_edge_attributes(H, "score")
+
+ 
+
     nx.draw(H, pos, with_labels=True, node_size=2000)
+    nx.draw_networkx_edge_labels(H, pos, edge_labels=edge_labels)
+
     plt.show()
 
 
-def show_most_common_traits(features, vocab, recommended, target):
+def show_most_common_traits(features, vocab, recommended, target,n):
     feature_list= []
     for t in recommended:
         feature_list.append(np.array(features[t[0]], dtype=int))
@@ -41,10 +51,19 @@ def show_most_common_traits(features, vocab, recommended, target):
         for i in range(len(f)):
             if(f[i]==1) and (target_vector[i]==1):
                 running.append(i)
-    top3 = Counter(running).most_common(3)
-
+    common_features_len = len(running)
+    if(n> common_features_len):
+        n = common_features_len
+    top3 = Counter(running).most_common(n)
+    df = pd.DataFrame({'Feature Name': [], 'Count': []})
+    
+    #agg_df = pd.concat([df, agg_df], ignore_index=True)
     for item, count in top3:
-        print(f"Trait {vocab[item]} was shared in common with other recommended users {count} times")
+        #print(f"Trait {vocab[item]} was shared in common with other recommended users {count} times")
+        df.loc[len(df)] = [vocab[item], count]
+    print(tabulate(df, headers='keys', tablefmt='psql', showindex=False))
+    return 1
+    
     
     
     
